b376a61838d846be89a429a454f0fe29690bc533
Samuel Perez Tovar <perezsamuelxzw@hotmail.com>
2025-02-25 20:58:12 -0500

    Optimized transition video and expanded the star background component to fill the screen

--- app/components/StarBackground.tsx
+++ app/components/StarBackground.tsx
@@ -47,7 +47,7 @@ export const StarBackground = (props: PointsInstancesProps) => {
 };
 
 export const StarsCanvas = () => (
-  <div className="w-full h-auto fixed inset-0 -z-10">
+  <div className="w-full h-screen fixed inset-0 -z-10">
     <Canvas camera={{ position: [0, 0, 1] }}>
       <Suspense fallback={null}>
         <StarBackground />
