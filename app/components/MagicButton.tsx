54a3641ec33a3359d4fd8c7758c374e0616e5279
Samuel Perez Tovar <perezsamuelxzw@hotmail.com>
2025-11-08 13:42:02 -0500

    Fix button width for compatibility

--- app/components/MagicButton.tsx
+++ app/components/MagicButton.tsx
@@ -15,7 +15,7 @@ const MagicButton = ({
 }) => {
   return (
     <button
-      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
+      className="relative inline-flex h-12 w-full md:w-50 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
       onClick={handleClick}
     >
       <span className="absolute inset-[-1000%] bg-[#FF751B]" />
